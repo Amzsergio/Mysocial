b82c15be6b0caba2e8f48f7f74e078e57a9ab597
Amzsergio <ac.sergioandres@gmail.com>
2023-02-14 22:44:36 -0500

    Register set up

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js"
 
 // CONFIGURATIONS
 
@@ -37,3 +38,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+//ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register)
+
+//MONGOOSE SET UP
+const PORT = process.env.PORT || 6001;
+ mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+ }).then(() => {
+    app.listen(PORT,  () => console.log(`Server port: ${PORT}`))
+ }).catch((error) => console.log(`${error} did not connect`))
